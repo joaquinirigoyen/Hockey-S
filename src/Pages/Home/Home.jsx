aaac958402976ba5c2e685c4f0c3619878716981
joaquin <joaquin.iri25@gmail.com>
2024-04-23 19:12:40 -0300

    agregando proyecto

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+
+    return (
+        <div className=" bg-slate-600 min-h-screen h-ful text-gray-200 flex items-center justify-center py-20 px-5 " >
+            <div className="container flex flex-col max-w-xl">
+
+            </div>
+        </div >
+    );
+};
+
+export default Home;
